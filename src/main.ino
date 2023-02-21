20e76a47dd860dd3d8e4cd22181a9fd87f88104a
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-02-21 19:15:39 +0300

    updated base code ,learned calibrate points

--- src/main.ino
+++ src/main.ino
@@ -1,23 +1,61 @@
-
 #define kbd_tr_tr
 #include "DigiKeyboard.h"
 
+// s1 read val:    161
+// s2 read val:    336
+// s3 read val:    506
+// s4 read val:    658
+// s5 read val:    838
+// s6 read val:    1023
+// button 1 pressed
+// button 2 pressed
+// button 3 pressed
+// button 4 pressed
+// button 5 pressed
+// button 6 pressed
+//
+
 void setup() {
-  // don't need to set anything up to use DigiKeyboard
-  DigiKeyboard.delay(2500); // wait some time before first run, to give target
-                            // time to initialize
-  // this is generally not necessary but with some older systems it seems to
-  // prevent missing the first character after a delay:
+  // disable input for 2,5 second for no startup errors
+  // and sending empty keystroke
+  DigiKeyboard.delay(2500);
   DigiKeyboard.sendKeyStroke(0);
 }
 
 void loop() {
-  // Type out this string letter by letter on the computer (assumes US-style
-  // keyboard)
-  DigiKeyboard.println("Hello Digispark!");
 
-  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
-  // if doing keyboard stuff because it keeps talking to the computer to make
-  // sure the computer knows the keyboard is alive and connected
-  DigiKeyboard.delay(5000);
+  int data = analogRead(1);
+  if (data > 100) {
+    // if button 6 pressed we do this operations
+    if (data > 1022) {
+      DigiKeyboard.print("button 6 pressed");
+    }
+    // if button 5 pressed we do this operations
+    else if (data > 837) {
+      DigiKeyboard.print("button 5 pressed");
+    }
+    // if button 4 pressed we do this operations
+    else if (data > 658) {
+      DigiKeyboard.print("button 4 pressed");
+    }
+    // if button 3 pressed we do this operations
+    else if (data > 504) {
+      DigiKeyboard.print("button 3 pressed");
+    }
+    // if button 2 pressed we do this operations
+    else if (data > 333) {
+      DigiKeyboard.print("button 2 pressed");
+    }
+    // if button 1 pressed we do this operations
+    else {
+      DigiKeyboard.print("button 1 pressed");
+    }
+
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    while (analogRead(1) > 100)
+      ;
+  }
+
+  // for ignoring analog read unstabilty to waiting very short time
+  DigiKeyboard.delay(50);
 }
